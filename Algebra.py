cb37105738c6da170a0be0ae6dbfcfa501174eae
Jaheim Archibald <jaheimarchibald@gmail.com>
2022-07-08 00:55:43 -0400

    Fix unittest 6

--- Algebra.py
+++ Algebra.py
@@ -2131,6 +2131,7 @@ def formatSpecialValues(solution: str) -> str:
                         .replace('0.5773502691896257', 'frac{sqrt{3}}{3}') \
                         .replace('1.7320508075688767', 'sqrt{3}')\
                         .replace('1.414213562373095', 'sqrt{2}')\
+                        .replace('1.4142135623730951', 'sqrt{2}')\
                         .replace('0.5000000000000001','frac{1}{2}')
 
     if 'frac' not in solution:
